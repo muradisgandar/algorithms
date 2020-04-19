4c1cc5249a304218ee7f3b8c71ba1eed22f174e4
muradisgandar <isgandarli_murad@mail.ru>
2020-04-19 16:40:46 +0400

    dynamic connectivity -  quick find algorithm

---
+++ Algorithms-1/Dynamic Connectivity/Quick Find/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        QuickFindUF uf = new QuickFindUF(n);
+        while (n > 0) {
+            int p = sc.nextInt();
+            int q = sc.nextInt();
+            if (!uf.connected(p, q)) {
+                uf.union(p, q);
+                System.out.println(p + " " + q);
+            }
+        }
+    }
+}
