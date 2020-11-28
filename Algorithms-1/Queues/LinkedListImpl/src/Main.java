6552b375a8eb55006375fb3115c5aa6579a6a614
muradisgandar <isgandarli_murad@mail.ru>
2020-11-28 14:32:48 +0400

    new file is added

--- Algorithms-1/Queues/LinkedListImpl/src/Main.java
+++ Algorithms-1/Queues/LinkedListImpl/src/Main.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        LinkedStackOfStrings stack = new LinkedStackOfStrings();
+        LinkedQueueOfStrings stack = new LinkedQueueOfStrings();
         Scanner sc = new Scanner(System.in);
 
         String s = sc.next();
         while (!s.isEmpty()) {
             if (s.equals("-")) {
-                System.out.println(stack.pop());
-            } else stack.push(s);
+                System.out.println(stack.dequeue());
+            } else stack.enqueue(s);
         }
     }
 }
