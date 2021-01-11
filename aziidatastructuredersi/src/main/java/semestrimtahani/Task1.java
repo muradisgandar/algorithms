ae338804534a77ecae9e5794d5d14df1edca0542
muradisgandar <isgandarli_murad@mail.ru>
2021-01-11 12:08:03 +0400

    final tasks

---
+++ aziidatastructuredersi/src/main/java/semestrimtahani/Task1.java
@@ -0,0 +1,34 @@
+package semestrimtahani;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(9);
+        numbers.add(2);
+        numbers.add(-5);
+        numbers.add(7);
+        numbers.add(-12);
+        numbers.add(13);
+
+        convert(numbers);
+
+    }
+
+    private static void convert(List<Integer> numbers) {
+        ArrayList<Integer> negativeList = new ArrayList<>();
+        for (int i = 0; i < numbers.size(); i++) {
+            if(numbers.get(i) > 0){
+                negativeList.add(-numbers.get(i));
+            }
+            else{
+                negativeList.add(numbers.get(i));
+            }
+        }
+
+        negativeList.forEach(System.out::println);
+    }
+}
---
+++ aziidatastructuredersi/src/main/java/semestrimtahani/Task2.java
@@ -0,0 +1,117 @@
+package semestrimtahani;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task2 {
+
+
+
+    public static void main(String[] args) {
+
+        List<Employee> employeeInfo = new ArrayList<>();
+
+        employeeInfo.add(new Employee("Murad",2000, "director"));
+        employeeInfo.add(new Employee("Vasif",2000, "technican"));
+        employeeInfo.add(new Employee("Abdulla",2000, "manager"));
+        employeeInfo.add(new Employee("Kerim",2000, "staff"));
+
+        List<ReturnedObject> returnedObjects = increaseSalary(20, employeeInfo);
+        printOldSalary(employeeInfo);
+        printNewSalary(returnedObjects);
+
+    }
+
+    private static List<ReturnedObject> increaseSalary(int percentage, List<Employee> employees){
+        List<ReturnedObject> list = new ArrayList<>();
+        int newSalary;
+        for(Employee e : employees){
+            int salary = e.getSalary();
+            newSalary = salary + salary * percentage/100;
+
+            list.add(new ReturnedObject(e.getName() , newSalary));
+        }
+
+        return list;
+
+    }
+
+    private static void printOldSalary(List<Employee> employees){
+        for(Employee e : employees){
+            System.out.println("name: " + e.getName() + " old salary: " + e.getSalary());
+        }
+    }
+
+    private static void printNewSalary(List<ReturnedObject> returnedObjects){
+        for(ReturnedObject e : returnedObjects){
+            System.out.println("name: " + e.getName() + " new salary: " + e.getSalary());
+        }
+    }
+
+    static class ReturnedObject {
+
+        private String name;
+        private Integer salary;
+
+        public ReturnedObject(String name, Integer salary) {
+            this.name = name;
+            this.salary = salary;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getSalary() {
+            return salary;
+        }
+
+        public void setSalary(Integer salary) {
+            this.salary = salary;
+        }
+    }
+
+    static class Employee {
+
+        private String name;
+        private Integer salary;
+        private String status;
+
+        public Employee(String name, Integer salary, String status) {
+            this.name = name;
+            this.salary = salary;
+            this.status = status;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getSalary() {
+            return salary;
+        }
+
+        public void setSalary(Integer salary) {
+            this.salary = salary;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+    }
+
+
+}
